ea0646aa50f6712f0351a0575cd54e0e42cd78ec
menta <jimementa@gmail.com>
2022-02-04 20:01:37 +0100

    comienzo

--- src/app/services/panel.service.ts
+++ src/app/services/panel.service.ts
@@ -6,6 +6,24 @@ import { Injectable } from '@angular/core';
 export class PanelService {
 
   constructor() { }
+  addToBudget($event){
+    // this.myPanelService.calcular($event)
 
+    const id = $event.target.value;
+    const isChecked = $event.target.checked;
+
+    this.orders.map((element) =>{
+    
+      if(element.id == id &&  element.selected !== isChecked){
+        this.total +=element.price
+      }
+      if(element.id == id &&  element.selected === isChecked){
+        this.total -=element.price
+      } 
+      console.log(this.total)
+      console.log(isChecked)
+    });
+  }
+  
   
 }
